6a3b9c8327a7d72dac46fe1dec4693179dc872a4
20211576 <k.gheytuli@student.tue.nl>
2024-05-10 21:14:05 +0200

    repo_commits class skeleton has been added

---
+++ repository_classes/repo_commits.py
@@ -0,0 +1,9 @@
+import requests
+
+class RepoCommits:
+    """
+    Class to get commits from a git repository
+    """
+    def __init__(self, repo_owner:str = None, repo_name:str = None, branch_name:str = "main", since:str = None, until:str = None, per_page:int = 100, page:int = 1):
+        pass
+    
