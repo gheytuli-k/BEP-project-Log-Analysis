d2d4b4b9bb3040b7f83f9d4afea729e0ab5cc282
20211576 <k.gheytuli@student.tue.nl>
2024-05-15 11:59:52 +0200

    add abstract methods from superclass

--- repository_classes/limit_rate_status.py
+++ repository_classes/limit_rate_status.py
@@ -1,8 +1,27 @@
 from repository_classes import RequestFromRepo
 
-class LimitRateStatus():
+class LimitRateStatus(RequestFromRepo):
     """
     A class for interacting with the GitHub API rate limit status.
     """
 
-    pass
+    def get_info(self) -> None:
+        """
+        Prints all the request attributes.
+        """
+
+        pass
+
+    def set_request_info(self, *args, **kwargs) -> None:
+        """
+        Set request information
+        """
+
+        pass
+
+    def make_request(self) -> None:
+        """
+        Makes a request to the repository with the set request information
+        """
+
+        pass
