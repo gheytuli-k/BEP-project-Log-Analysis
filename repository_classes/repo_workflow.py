0ba9c36cf4523a6a6a219657fbdc975947aff966
20211576 <k.gheytuli@student.tue.nl>
2024-05-20 17:21:38 +0200

    RepoWorkflow skeleton has been added

---
+++ repository_classes/repo_workflow.py
@@ -0,0 +1,14 @@
+from repository_classes import RequestFromRepo
+import requests
+
+
+class RepoWorkflow(RequestFromRepo):
+
+    def get_info(self) -> None:
+        pass
+
+    def set_request_info(self, *args, **kwargs) -> None:
+        pass
+
+    def make_request(self) -> None:
+        pass
