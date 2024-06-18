61ae4d80bab1f96398063ab6e005870ff4193126
20211576 <k.gheytuli@student.tue.nl>
2024-06-18 09:33:16 +0200

    init for repo classes added

--- repository_classes/__init__.py
+++ repository_classes/__init__.py
@@ -1,3 +1,6 @@
 from .request_from_repo import RequestFromRepo
 from .repo_commits import RepoCommits
 from .repo_branch import RepoBranch
+from .limit_rate_status import LimitRateStatus
+from .repo_workflow import RepoWorkflow
+from .repo_jobs_workflow import RepoJobsWorkflow
